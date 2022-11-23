d96fb200e0866ad6159c64e9cd3fe046efabf8d2
Kser1x <115347348+Kser1x@users.noreply.github.com>
2022-11-23 16:21:34 +0300

    "Начальная ступень бота"

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const token = "5824120026:AAG3T8pW_xP2zuQ5Ga9skkX5dvqWD2y5eKo"
 
 func main() {
-	fmt.Println("Hello bot")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
 
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // If we got a message
+			continue
+		}
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote:"+update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
